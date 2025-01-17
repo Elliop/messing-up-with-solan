f4f617b26d5173a9b7c3a1192d4c66f2e4464da0
Elliop <abdelmouhayminegx@gmail.com>
2025-01-17 16:36:20 +0100

    rename component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
 
-const Walletconnect = dynamic(() => import("../components/WalletConnect"), {
+const Walletconnect = dynamic(() => import("../components/ConnectWallet"), {
   ssr: false,
 });
 
